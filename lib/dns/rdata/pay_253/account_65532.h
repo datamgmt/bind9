d9b5480086a165d09fbfab10c0cd225f39307606
David M Walker <davidw@datamgmt.com>
2020-01-02 16:22:09 +0000

    Update account_65532.h

--- lib/dns/rdata/pay_253/account_65532.h
+++ lib/dns/rdata/pay_253/account_65532.h
@@ -21,4 +21,31 @@ typedef struct dns_rdata_account {
 	dns_name_t		account;
 } dns_rdata_account_t;
 
+/*
+
+// Possible Replacement Structure
+
+typedef struct dns_rdata_account {
+	dns_rdatacommon_t	common;
+	isc_mem_t		*mctx;
+	// The rdata is now pref, scheme_name, account_token, currency_code
+	// pref: unisgned 16 bit int for compatibility with MX format
+	uint16_t		pref;
+	// scheme_name_length: unsignted 8 bit int - valid values 1-16
+	uint8_t			scheme_name_len;
+	// scheme_name: ascii string 1-15 chars long
+	pay_scheme_name_t		scheme_name;
+	// account_name_length: unsigned 8-bit int - valid values 1-192
+	uint8_t			account_token_len
+	// account_token: ascii string 1-192 chars long
+    pay_account_token_t     account_token;
+	// xurrency_code: ascii string 3 chars long, valid values must be part of ISI 4217
+	// See https://www.iso.org/iso-4217-currency-codes.html 
+	// and https://en.wikipedia.org/wiki/ISO_4217
+    pay_currency_code_t	    currency_code;
+	// The total RDAT size can vary from 10 to 215 in length which is less than <255 max recommended
+} dns_rdata_account_t;
+
+*/
+
 #endif /* PAY_253_ACCOUNT_65532_H */
